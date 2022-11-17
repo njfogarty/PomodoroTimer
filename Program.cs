8bc7f7b019c783f4aaeb702e0bc62921122aa672
Noah Fogarty <nojosoft@gmail.com>
2022-11-17 10:23:59 -0500

    added time remaining
    the console window will now display the time remaining in the current cycle

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ Console.Title = "Pomodoro Timer";
 
 // CHANGELOG: v1.1.2
 // Added: Option to repeat timer with different settings.
-// Bug fix: Made it so you can't specify a value of 0 or below for some configuration settings.
+// Bugfix: Made it so you can't specify a value of 0 or below for some configuration settings.
 
 // CHANGELOG: v1.1.0 
 // Added: Option to have the program ask user to press a key before beginning each timer. User can toggle this option during the initial configuration phase.
@@ -23,8 +23,10 @@ Console.Title = "Pomodoro Timer";
 #endregion
 
 #region variables
-float workMinutes = 25 * 60000;
-float breakMinutes = 5 * 60000;
+int workMinutes = 25;
+int breakMinutes = 5;
+int minutesWorked = workMinutes;
+int secondCounter = 0;
 int repeatAmount = 4;
 int amountRepeated = 0;
 string userConfigChoice = "Yes";
@@ -64,8 +66,8 @@ while (repeatConfig)
             case "yes":
             case "Y":
             case "y":
-                workMinutes = 25 * 60000;
-                breakMinutes = 5 * 60000;
+                workMinutes = 25;
+                breakMinutes = 5;
                 repeatAmount = 4;
                 invalidCheckPassed = true;
                 break;
@@ -75,10 +77,10 @@ while (repeatConfig)
             case "N":
             case "n":
                 Console.Write("\nHow many minutes would you like each work session to be?: ");
-                workMinutes = Convert.ToSingle(Console.ReadLine()) * 60000;
+                workMinutes = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("\nHow many minutes would you like each break session to be?: ");
-                breakMinutes = Convert.ToSingle(Console.ReadLine()) * 60000;
+                breakMinutes = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("\nHow many times would you like to repeat?: ");
                 repeatAmount = Convert.ToInt32(Console.ReadLine());
@@ -137,7 +139,8 @@ while (repeatConfig)
 
         while (amountRepeated != repeatAmount)
         {
-            // Work timer.
+            minutesWorked = workMinutes;
+            
             if (shouldWait)
             {
                 Console.Clear();
@@ -150,12 +153,30 @@ while (repeatConfig)
             Console.Clear();
             Console.Title = "Pomodoro - Working...";
             
-            Console.WriteLine("Currently working...");
-            
-            Thread.Sleep((int)workMinutes);
+            // Work timer.
+            while (minutesWorked >= 0)
+            {
+                if (minutesWorked == 1 && secondCounter == 1) 
+                    Console.Write($"\r{minutesWorked} minute and {secondCounter} second left.     ");
+                else if (minutesWorked == 1) 
+                    Console.Write($"\r{minutesWorked} minute and {secondCounter} seconds left.     ");
+                else if (secondCounter == 1) 
+                    Console.Write($"\r{minutesWorked} minutes and {secondCounter} second left.     ");
+                else 
+                    Console.Write($"\r{minutesWorked} minutes and {secondCounter} seconds left.     ");
+                if (secondCounter == 0)
+                {
+                    secondCounter = 60;
+                    minutesWorked--;
+                }
+                Thread.Sleep(1000);
+                secondCounter--;
+            }
+
             Console.Beep(440, 1000);
 
-            // Break timer.
+            secondCounter = 0;
+
             if (shouldWait)
             {
                 Console.Clear();
@@ -166,12 +187,32 @@ while (repeatConfig)
             }
 
             Console.Clear();
-            Console.Title = "Pomodoro - Breaking...";
+            Console.Title = "Pomodoro - Resting...";
             
-            Console.WriteLine("Currently breaking...");
+            // Break timer.
+            minutesWorked = breakMinutes;
+            while (minutesWorked >= 0)
+            {
+                if (minutesWorked == 1 && secondCounter == 1) 
+                    Console.Write($"\r{minutesWorked} minute and {secondCounter} second left.     ");
+                else if (minutesWorked == 1) 
+                    Console.Write($"\r{minutesWorked} minute and {secondCounter} seconds left.     ");
+                else if (secondCounter == 1) 
+                    Console.Write($"\r{minutesWorked} minutes and {secondCounter} second left.     ");
+                else 
+                    Console.Write($"\r{minutesWorked} minutes and {secondCounter} seconds left.     ");
+                if (secondCounter == 0)
+                {
+                    secondCounter = 60;
+                    minutesWorked--;
+                }
+                Thread.Sleep(1000);
+                secondCounter--;
+            }
             
-            Thread.Sleep((int)breakMinutes);
             Console.Beep(220, 1000);
+
+            secondCounter = 0;
             
             amountRepeated++;
         }
